432e34e3365a946e6edb3ad85fa0f56464d93664
Isaac Do <huydo11813@gmail.com>
2022-11-12 17:59:34 -0600

    Update App.js

--- hackutd2022-app/src/App.js
+++ hackutd2022-app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen p-40 bg-indigo-400 text-center">
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
