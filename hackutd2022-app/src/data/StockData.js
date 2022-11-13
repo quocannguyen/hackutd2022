b2075c529064a7e5e6417495e8be502b0f6469bb
quocannguyen <78980126+quocannguyen@users.noreply.github.com>
2022-11-12 19:03:39 -0600

    Complete StockData and function getStockDataArrayFrom

--- hackutd2022-app/src/data/StockData.js
+++ hackutd2022-app/src/data/StockData.js
@@ -1,24 +1,26 @@
 class StockData {
-    open
-    high
-    low;
-    close;
-    volume;
-    constructor(open, high, low, close, volume){
+    constructor(open, high, low, close, volume, timeStamp){
         this.open = open;
         this.high = high;
         this.low = low;
         this.close = close;
         this.volume = volume;
+        this.timeStamp = timeStamp
     }
-
 }
 
-function makeArray(){
-    let stocks = new Array(StockData);
-    for (let i = 0; i < 20; i++) {
-        stocks.push();
+export function getStockDataArrayFrom(dataset){
+    let stockDataArray = new Array(StockData);
+    for (const timeStamp in dataset) {
+        let open = dataset[timeStamp]["1. open"]
+        let high = dataset[timeStamp]["2. high"]
+        let low = dataset[timeStamp]["3. low"]
+        let close = dataset[timeStamp]["4. close"]
+        let volume = dataset[timeStamp]["5. volume"]
+        let stockData = new StockData(open, high, low, close, volume, timeStamp)
+        stockDataArray.push(stockData)
     }
+    return stockDataArray
 }
 
 export default StockData;
